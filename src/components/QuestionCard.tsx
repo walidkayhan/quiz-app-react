bdea5a2ad6c511cdea9762657fadb180cbc1e631
Walid <walid.kayhan@mohawkcollege.ca>
2020-07-22 00:26:45 -0400

    Created question card component

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+//Styles
+import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
+
+//Types
+import { AnswerObject } from "../App";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => (
+  <Wrapper>
+    <p className="number">
+      Question: {questionNumber} / {totalQuestions}
+    </p>
+
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+
+    <div>
+      {answers.map((answer) => (
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={!!userAnswer} onClick={callback} value={answer}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
+    </div>
+  </Wrapper>
+);
+
+export default QuestionCard;
